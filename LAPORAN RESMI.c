b746ca4da42f397d7ac6618a87aafc8890872b47
hyhnuka <156576309+hyhnuka@users.noreply.github.com>
2024-01-15 18:40:35 +0700

    Add files via upload

---
+++ LAPORAN RESMI.c
@@ -0,0 +1,64 @@
+//D. LAPORAN RESMI
+//2a.
+/*
+#include <stdio.h>
+int main()
+{
+	int jawab, hasil;
+	jawab = 100;
+	hasil = jawab - 10;
+	printf("Jawabannya adalah %d\n", hasil + 6);
+
+}
+*/
+//2b.
+/*
+main()
+{
+	int value1, value2, sum;
+	value1 = 35;
+	value2 = 18;
+	sum = value1 + value2;
+	printf("The sum of %d and %d is %d\n", value1,value2,sum);
+}
+*/
+//2c.
+/*
+main ()
+{
+	int jumlah;
+	// PERHITUNGAN HASIL
+	jumlah = 25 + 37 - 19;
+	//TAMPILKAN HASIL
+	printf("Berapa hasil perhitungan 25 + 37 - 19 ?\n");
+	printf("Jawabannya adalah %d\n", jumlah);
+}
+*/
+//2d.
+/*
+main()
+{
+	int a, b, sum, sqA, sqB;
+	float avg;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	avg = (float)( a + b ) / 2;
+	sqA = a*a;
+	sqB = b*b;
+	printf("a + b = %d\n", sum);
+	printf("rata-rata nilai a dan b adalah %.1lf\n", avg);
+	printf("nilai kuaderat a adalah %d dan nilai kudrat b adalah %d\n", sqA, sqB);
+}
+*/
+//2e.
+#include <stdio.h>
+main ()
+{
+	float a, b, c;
+	a = 3;
+	b = 4.0;
+	c = a * a + b * b;
+	printf("c * c = %.6lf", c);
+}
+
+
